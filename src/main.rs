b43eb2571d6dd179c17b55350ad92f57ca46e50b
Giancarlo Giuffra <giancarlo.giuffra.moncayo@gmail.com>
2025-12-23 17:48:02 +0100

    Implementation until after error handling

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,85 @@
-use std::io::Read;
-use serde::Serialize;
+mod transaction;
 
-#[derive(Debug, Serialize)]
-struct Transaction {
-    version: u32,
-    inputs: Vec<Input>,
-    outputs: Vec<Output>,
-}
+use std::io::{Error, Read};
+use std::error::Error as StdError;
+use sha2::Digest;
+use transaction::{Amount, Input, Output, Transaction};
+use crate::transaction::{Script, TxId};
 
-#[derive(Debug, Serialize)]
-struct Input {
-    txid: [u8; 32],
-    output_index: u32,
-    script_sig: Vec<u8>,
-    sequence: u32,
-}
-
-#[derive(Debug, Serialize)]
-struct Output {
-    amount: f64,
-    script_pubkey: Vec<u8>,
-}
-
-#[derive(Debug, Serialize)]
-struct Amount(u64);
-
-impl Amount {
-    pub fn to_btc(&self) -> f64{
-        self.0 as f64 / 100_000_000.0
-    }
-}
-
-fn read_compact_size(transaction_bytes: &mut &[u8]) -> u64 {
+fn read_compact_size(transaction_bytes: &mut &[u8]) -> Result<u64, Error> {
     let mut compact_size = [0;1];
-    transaction_bytes.read(&mut compact_size).unwrap();
+    transaction_bytes.read(&mut compact_size)?;
 
     match compact_size[0] {
-        0..=252 => compact_size[0] as u64,
+        0..=252 => Ok(compact_size[0] as u64),
         253 => {
             let mut buffer = [0;2];
-            transaction_bytes.read(&mut buffer).unwrap();
-            u16::from_le_bytes(buffer) as u64
+            transaction_bytes.read(&mut buffer)?;
+            Ok(u16::from_le_bytes(buffer) as u64)
         },
         254 => {
             let mut buffer = [0;4];
-            transaction_bytes.read(&mut buffer).unwrap();
-            u32::from_le_bytes(buffer) as u64
+            transaction_bytes.read(&mut buffer)?;
+            Ok(u32::from_le_bytes(buffer) as u64)
         },
         255 => {
             let mut buffer = [0;8];
-            transaction_bytes.read(&mut buffer).unwrap();
-            u64::from_le_bytes(buffer)
+            transaction_bytes.read(&mut buffer)?;
+            Ok(u64::from_le_bytes(buffer))
         }
     }
 }
 
-fn read_u32(transaction_bytes: &mut &[u8]) -> u32 {
+fn read_u32(transaction_bytes: &mut &[u8]) -> Result<u32, Error> {
     let mut version_bytes = [0;4];
-    transaction_bytes.read(&mut version_bytes).unwrap();
-    u32::from_le_bytes(version_bytes)
+    transaction_bytes.read(&mut version_bytes)?;
+    Ok(u32::from_le_bytes(version_bytes))
 }
 
-fn read_amount(transaction_bytes: &mut &[u8]) -> Amount {
+fn read_amount(transaction_bytes: &mut &[u8]) -> Result<Amount, Error> {
     let mut version_bytes = [0;8];
-    transaction_bytes.read(&mut version_bytes).unwrap();
-    Amount(u64::from_le_bytes(version_bytes))
+    transaction_bytes.read(&mut version_bytes)?;
+    Ok(Amount::from_sat(u64::from_le_bytes(version_bytes)))
 }
 
-fn read_txid(transaction_bytes: &mut &[u8]) -> [u8; 32] {
+fn read_txid(transaction_bytes: &mut &[u8]) -> Result<TxId, Error> {
     let mut txid_bytes = [0;32];
-    transaction_bytes.read(&mut txid_bytes).unwrap();
-    txid_bytes.reverse();
-    txid_bytes
+    transaction_bytes.read(&mut txid_bytes)?;
+    Ok(TxId::from_bytes(txid_bytes))
 }
 
-fn read_script(transaction_bytes: &mut &[u8]) -> Vec<u8> {
-    let script_size = read_compact_size(transaction_bytes);
+fn read_script(transaction_bytes: &mut &[u8]) -> Result<Script, Error> {
+    let script_size = read_compact_size(transaction_bytes)?;
     let mut script_sig = vec![0; script_size as usize];
-    transaction_bytes.read(&mut script_sig).unwrap();
-    script_sig
+    transaction_bytes.read(&mut script_sig)?;
+    Ok(Script::from_vec(script_sig))
 }
 
-fn main() {
-    let transaction_hex = "01000000015180fff4155787703d10f03cca1566794516ac65a67764e571dc9c34931f321d050000006a473044022100a98648381f405a6882989faa500147c7cb9f4ce03e912d18529fb3609e243a47021f798214efe634e8c47e158edae534f5652b98ce1bd3693fa95dcdd2c699d987012102bd63ab2a6215bdd16d554ea3fd5d83843bff9c76e0b8c6c150d58ee6ca7ea525ffffffff01a08b2013000000001976a9142f58e6245481be77894d5f0f0e2641decdafc44788ac00000000";
-    let transaction_bytes = hex::decode(transaction_hex).unwrap();
+fn hash_raw_transaction(raw_transaction: &[u8]) -> TxId {
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(raw_transaction);
+    let hash1 = hasher.finalize();
+
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(hash1);
+    let hash2 = hasher.finalize();
+
+    TxId::from_bytes(hash2.into())
+}
+
+fn decode(transaction_hex: String) -> Result<String, Box<dyn StdError>>{
+    let transaction_bytes = hex::decode(transaction_hex)?;
     let mut bytes_slice = transaction_bytes.as_slice();
 
-    let version = read_u32(&mut bytes_slice);
+    let version = read_u32(&mut bytes_slice)?;
 
-    let input_count = read_compact_size(&mut bytes_slice);
+    let input_count = read_compact_size(&mut bytes_slice)?;
     let mut inputs = vec![];
     for _ in 0..input_count {
-        let txid = read_txid(&mut bytes_slice);
-        let output_index = read_u32(&mut bytes_slice);
-        let script_sig = read_script(&mut bytes_slice);
-        let sequence = read_u32(&mut bytes_slice);
+        let txid = read_txid(&mut bytes_slice)?;
+        let output_index = read_u32(&mut bytes_slice)?;
+        let script_sig = read_script(&mut bytes_slice)?;
+        let sequence = read_u32(&mut bytes_slice)?;
 
         inputs.push(Input {
             txid,
@@ -104,11 +89,11 @@ fn main() {
         });
     }
 
-    let output_count = read_compact_size(&mut bytes_slice);
+    let output_count = read_compact_size(&mut bytes_slice)?;
     let mut outputs = vec![];
     for _ in 0..output_count {
-        let amount = read_amount(&mut bytes_slice).to_btc();
-        let script_pubkey = read_script(&mut bytes_slice);
+        let amount = read_amount(&mut bytes_slice)?;
+        let script_pubkey = read_script(&mut bytes_slice)?;
 
         outputs.push(Output {
             amount,
@@ -116,40 +101,56 @@ fn main() {
         });
     }
 
+    let lock_time = read_u32(&mut bytes_slice)?;
+    let transaction_id = hash_raw_transaction(&transaction_bytes);
+
     let transaction = Transaction {
+        transaction_id,
         version,
         inputs,
-        outputs
+        outputs,
+        lock_time
     };
-    println!("Transaction: {}", serde_json::to_string_pretty(&transaction).unwrap());
+    Ok(serde_json::to_string_pretty(&transaction)?)
+}
+
+fn main() {
+    let transaction_hex = "01000000015180fff4155787703d10f03cca1566794516ac65a67764e571dc9c34931f321d050000006a473044022100a98648381f405a6882989faa500147c7cb9f4ce03e912d18529fb3609e243a47021f798214efe634e8c47e158edae534f5652b98ce1bd3693fa95dcdd2c699d987012102bd63ab2a6215bdd16d554ea3fd5d83843bff9c76e0b8c6c150d58ee6ca7ea525ffffffff01a08b2013000000001976a9142f58e6245481be77894d5f0f0e2641decdafc44788ac00000000";
+    match decode(transaction_hex.to_string()) {
+        Ok(decoded) => println!("Transaction: {}", decoded),
+        Err(error) => eprintln!("{}", error)
+    }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::error::Error;
     use crate::read_compact_size;
 
     #[test]
-    fn read_compact_size_test() {
+    fn read_compact_size_test() -> Result<(), Box<dyn Error>> {
         let mut bytes = [1_u8].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 1);
 
         let mut bytes = [253_u8, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256);
 
         let mut bytes = [254_u8, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(3));
 
         let mut bytes = [255_u8, 0, 0, 0, 0, 0, 0, 0, 1].as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 256_u64.pow(7));
 
         let hex = "fd204e";
-        let decoded = hex::decode(hex).unwrap();
+        let decoded = hex::decode(hex)?;
         let mut bytes = decoded.as_slice();
-        let count = read_compact_size(&mut bytes);
+        let count = read_compact_size(&mut bytes)?;
         assert_eq!(count, 20_000);
+
+        Ok(())
     }
 }
---
+++ src/transaction.rs
@@ -0,0 +1,87 @@
+use serde::{Serialize, Serializer};
+
+#[derive(Debug, Serialize)]
+pub struct Transaction {
+    pub transaction_id: TxId,
+    pub(crate) version: u32,
+    pub(crate) inputs: Vec<Input>,
+    pub(crate) outputs: Vec<Output>,
+    pub lock_time: u32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Input {
+    pub(crate) txid: TxId,
+    pub(crate) output_index: u32,
+    pub(crate) script_sig: Script,
+    pub(crate) sequence: u32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Output {
+    #[serde(serialize_with = "as_btc")]
+    pub(crate) amount: Amount,
+    pub(crate) script_pubkey: Script,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Amount(u64);
+
+impl Amount {
+    pub fn from_sat(satoshi: u64) -> Amount {
+        Amount(satoshi)
+    }
+}
+
+#[derive(Debug)]
+pub struct TxId([u8; 32]);
+
+impl TxId {
+    pub fn from_bytes(bytes: [u8; 32]) -> TxId {
+        TxId(bytes)
+    }
+}
+
+impl Serialize for TxId {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        let mut bytes = self.0.clone();
+        bytes.reverse();
+        serializer.serialize_str(&hex::encode(&bytes))
+    }
+}
+
+#[derive(Debug)]
+pub struct Script(Vec<u8>);
+
+impl Script {
+    pub fn from_vec(vec: Vec<u8>) -> Script {
+        Script(vec)
+    }
+}
+
+impl Serialize for Script {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer
+    {
+        serializer.serialize_str(&hex::encode(self.0.as_slice()))
+    }
+}
+
+impl BitcoinValue for Amount {
+    fn to_btc(&self) -> f64{
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+trait BitcoinValue {
+    fn to_btc(&self) -> f64;
+}
+
+fn as_btc<S: Serializer, T: BitcoinValue>(t : &T, s: S) -> Result<S::Ok, S::Error> {
+    let btc = t.to_btc();
+    s.serialize_f64(btc)
+}
